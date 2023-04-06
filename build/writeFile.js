5d0cd7e98a2013a7b2d1fce1c94d8cfdbe3d6008
yangss <1332551701@qq.com>
2023-04-07 00:33:47 +0800

    打包缩进条获赠

--- build/writeFile.js
+++ build/writeFile.js
@@ -23,9 +23,9 @@ function creatPackageJson() {
     jsonData.devDependencies && delete jsonData.devDependencies
     jsonData.main = 'openai-self.cjs.js'
     jsonData.module = 'openai-self.esm.js'
-    fs.writeFile('./dist/package.json', JSON.stringify(jsonData, null, 4), function (err) {
-        if (err) console.error(err);
-        console.log('打包完成，请进入dist目录，确认package.json文件信息，然后进行发布！');
+    fs.writeFile('./dist/package.json', JSON.stringify(jsonData, null, 2), function (err) {
+      if (err) console.error(err);
+      console.log('打包完成，请进入dist目录，确认package.json文件信息，然后进行发布！');
     });
 }
 copyFileTo('./NPM_README.md', './dist/README.md')
